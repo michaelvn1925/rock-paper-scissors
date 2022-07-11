c1127caa303149547c6ba74a47463e6fc8cbb8c8
michaelvn1925 <59118669+michaelvn1925@users.noreply.github.com>
2022-07-11 06:32:49 -0700

    Update game_test.py

--- game_test.py
+++ game_test.py
@@ -30,15 +30,16 @@ scissors = '''
 game_image = [rock, paper, scissors]
 
 user_choice = int(input("What do you chioose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3:
+  print("You typed an invalid number.")
+else:
 print(game_image[user_choice])
 
 comp_choice = random.randint(0, 2)
 print(f"Computer choose {comp_choice}")
 print(game_image[comp_choice])
 
-if user_choice >= 3:
-  print("You typed an invalid number.")
-elif user_choice == 0 and comp_choice == 2:
+if user_choice == 0 and comp_choice == 2:
   print("You Win!")
 elif comp_choice == 0 and user_choice == 2:
   print("You lose!")
